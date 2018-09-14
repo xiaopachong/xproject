cfc90d7bb1f211e65ec417a1cc697c2fd1492ad4
xiaopachong <469103780@qq.com>
2018-09-14 21:06:02 +0800

    提交protobuf测试结果

--- updateol/src/test/java/com/study/test/protobuf/TestProtobufProtocol.java
+++ updateol/src/test/java/com/study/test/protobuf/TestProtobufProtocol.java
@@ -1,6 +1,7 @@
 package com.study.test.protobuf;
 
-import com.study.protobuf.newprotocol.ProtobufProtocol;
+import com.study.protobuf.newprotocol.NewProtobufProtocol;
+import com.study.protobuf.oldprotocol.OldProtobufProtocol;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,40 +26,45 @@ public class TestProtobufProtocol {
     @DisplayName("序列化protobuf对象到本地文件")
     void testSerializationWithOldProtocol() throws IOException {
         //构造测试对象
-        List<ProtobufProtocol.Person> list = new ArrayList<ProtobufProtocol.Person>();
-        ProtobufProtocol.Person person = ProtobufProtocol.Person.newBuilder()
+        List<OldProtobufProtocol.Person> list = new ArrayList<OldProtobufProtocol.Person>();
+        OldProtobufProtocol.Person person = OldProtobufProtocol.Person.newBuilder()
                 .setEmail(email_test).setName(name_test).setId(id_test).build();
         list.add(person);
-        ProtobufProtocol.AddressBook addressBook = ProtobufProtocol.AddressBook.newBuilder()
+        OldProtobufProtocol.AddressBook addressBook = OldProtobufProtocol.AddressBook.newBuilder()
                 .addAllPerson(list).build();
         System.out.println(addressBook);
 
         //将测试对象写到本地文件
-        ObjectOutputStream outputStream = null;
+        OutputStream os = null;
         try {
             File file = new File(file_path_test);
-            outputStream = new ObjectOutputStream(new FileOutputStream(file));
-            outputStream.writeObject(addressBook);
-            outputStream.flush();
+            os = new FileOutputStream(file);
+            os.write(addressBook.toByteArray());
+            os.flush();
         } finally {
-            if (outputStream != null) {
-                outputStream.close();
+            if (os != null) {
+                os.close();
             }
         }
     }
 
     @Test
     @DisplayName("使用新协议版本，反序列化本地测试对象")
-        //新协议，旧协议必须使用同一个包名,同一个类名，否则会报java.lang.ClassCastException.
-    void testDeserializationWithNewProtocol() throws IOException, ClassNotFoundException {
+    void testDeserializationWithNewProtocol() throws IOException {
         //读本地文件对象
-        ObjectInputStream ois = null;
+        InputStream is = null;
         try {
             File file = new File(file_path_test);
-            ois = new ObjectInputStream(new FileInputStream(file));
+            is = new FileInputStream(file);
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            byte[] b = new byte[1024];
+            int len = 0;
+            while ((len = is.read(b)) != -1) {
+                os.write(b, 0, len);
+            }
             //使用新版本协议
-            ProtobufProtocol.AddressBook addressBook =
-                    (ProtobufProtocol.AddressBook) ois.readObject();
+            NewProtobufProtocol.AddressBook addressBook =
+                    NewProtobufProtocol.AddressBook.parseFrom(os.toByteArray());
             assert addressBook != null;
             assert addressBook.getPersonList().size() > 0 && addressBook.getPersonList().get(0) != null;
             assert addressBook.getPersonList().get(0).getId() == id_test;
@@ -66,8 +72,8 @@ public class TestProtobufProtocol {
             assert addressBook.getPersonList().get(0).getName().equals(name_test);
             System.out.println(addressBook);
         } finally {
-            if (ois != null) {
-                ois.close();
+            if (is != null) {
+                is.close();
             }
         }
     }
